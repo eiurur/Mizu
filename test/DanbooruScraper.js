8addabb6be0727071064e5e915a9b7ab2e445713
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-03-27 10:20:20 +0900

    ok danbooru

---
+++ test/DanbooruScraper.js
@@ -0,0 +1,68 @@
+const _ = require('lodash');
+const assert = require('power-assert');
+const Mizu = require('../');
+
+describe('DanbooruScraper', () => {
+  it('should return daily illustList when pass {term: "day"}', (done) => {
+    const opts = {
+      name: 'danbooru',
+      term: 'day',
+    };
+    const danbooru = Mizu.createScpraper(opts);
+    danbooru.crawl()
+    .then( illustList => {
+      assert(_.isArray(illustList));
+      // assert(_.isString(illustList[1].title));
+      done();
+    });
+  });
+
+  it('should return weekly illustList when pass {term: "week"}', (done) => {
+    const opts = {
+      name: 'danbooru',
+      term: 'week',
+      directory: 'danbooru_test_images_week',
+    };
+    const danbooru = Mizu.createScpraper(opts);
+    danbooru.crawl()
+    .then( illustList => {
+      assert(_.isArray(illustList));
+      assert(_.isString(illustList[1].title));
+
+      return danbooru.downloadIllusts();
+    })
+    .then( illustList => {
+      assert(_.isArray(illustList));
+      assert(_.isString(illustList[1].filename));
+      done();
+    });
+  });
+
+  it('should return weekly illustList when pass {term: "month"}', (done) => {
+    const opts = {
+      name: 'danbooru',
+      term: 'month',
+    };
+    const danbooru = Mizu.createScpraper(opts);
+    danbooru.crawl()
+    .then( illustList => {
+      assert(_.isArray(illustList));
+      assert(_.isString(illustList[1].title));
+      done();
+    });
+  });
+
+  // it('should return weekly illustList when pass {term: "year"}', (done) => {
+  //   const opts = {
+  //     name: 'danbooru',
+  //     term: 'year',
+  //   };
+  //   const danbooru = Mizu.createScpraper(opts);
+  //   danbooru.crawl()
+  //   .then( illustList => {
+  //     assert(_.isArray(illustList));
+  //     assert(_.isString(illustList[1].title));
+  //     done();
+  //   });
+  // });
+});
