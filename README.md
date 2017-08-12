38f21d26befd18323e953f087aa2bbb7aa1a50fd
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2017-08-12 23:12:07 +0900

    [Change] - file name to be saved to hash of imagee

--- README.md
+++ README.md
@@ -97,6 +97,34 @@ sankaku_complex.prev(2, 'week').turnPage(1).crawl()
 
 ```
 
+### Output (Example)
+
+```js
+const danbooru = Mizu.createCrawler(params);
+danbooru.crawl()
+.then( _ => danbooru.download() )
+.then( illustList => console.log(illustList) )
+
+↓
+
+{  
+    title:'2girls apron arched_back arm_up artoria_pendragon_(all) ass bikini black_bikini blonde_hair blush breasts closed_mouth cowboy_shot eyebrows_visible_through_hair fate/grand_order fate_(series) green_eyes hair_between_eyes hair_intakes highres hood hooded_jacket hoodie jacket light_brown_hair long_hair long_sleeves looking_at_viewer maid_headdress medium_breasts mop multiple_girls nail_polish nero_claudius_(swimsuit_caster)_(fate) open_clothes open_hoodie open_jacket saber_alter saber_extra side-tie_bikini sideboob simple_background smile striped striped_bikini swimsuit sword twintails v waist_apron weapon white_background yang-do yellow_eyes',
+    sourceURL:'https://danbooru.donmai.us/posts/2817083',
+    largeImgURL:'https://danbooru.donmai.us/data/__artoria_pendragon_nero_claudius_saber_alter_and_saber_extra_fate_grand_order_and_fate_series_drawn_by_yang_do__c149cb7a38ae80e42db0a05d4fe261cc.jpg',
+    mediumImgURL:'https://danbooru.donmai.us/data/sample/__artoria_pendragon_nero_claudius_saber_alter_and_saber_extra_fate_grand_order_and_fate_series_drawn_by_yang_do__sample-c149cb7a38ae80e42db0a05d4fe261cc.jpg',
+    url:'https://danbooru.donmai.us/data/sample/__artoria_pendragon_nero_claudius_saber_alter_and_saber_extra_fate_grand_order_and_fate_series_drawn_by_yang_do__sample-c149cb7a38ae80e42db0a05d4fe261cc.jpg',
+    ext:'.jpg'
+},
+{  
+    title:'1girl alexmaster arm_support bangs beach bikini blonde_hair blue_eyes blush breasts closed_mouth collarbone day eyebrows_visible_through_hair frown groin hair_ribbon leaning_back long_hair looking_at_viewer navel original outdoors palm_tree partially_visible_vulva pink_bikini red_ribbon ribbon shiny shiny_skin small_breasts spread_legs stomach sweat swimsuit thighs towel tree twintails umbrella wavy_mouth',
+    sourceURL:'https://danbooru.donmai.us/posts/2816002',
+    largeImgURL:'https://danbooru.donmai.us/data/__original_drawn_by_alexmaster__f53ddbc12565036e0ce803419c079696.jpg',
+    mediumImgURL:'https://danbooru.donmai.us/data/__original_drawn_by_alexmaster__f53ddbc12565036e0ce803419c079696.jpg',
+    url:'https://danbooru.donmai.us/data/__original_drawn_by_alexmaster__f53ddbc12565036e0ce803419c079696.jpg',
+    ext:'.jpg'
+},
+```
+
 # Support
 
 - [x] danbooru
--- test/DanbooruScraper.js
+++ test/DanbooruScraper.js
@@ -1,123 +1,164 @@
-'use strict';
+"use strict";
 
-const _      = require('lodash');
-const moment = require('moment');
-const assert = require('power-assert');
-const Mizu   = require('../');
+const _ = require("lodash");
+const moment = require("moment");
+const assert = require("power-assert");
+const Mizu = require("../");
 
-describe('DanbooruScraper', () => {
-  it('should return hostname(danbooru.donmai.us) when get hostname', () => {
+describe("DanbooruScraper", () => {
+  it("should return hostname(danbooru.donmai.us) when get hostname", () => {
     const opts = {
-      name: 'danbooru',
-      term: 'day',
+      name: "danbooru",
+      term: "day"
     };
     const danbooru = Mizu.createCrawler(opts);
-    assert(danbooru.hostname === 'danbooru.donmai.us');
+    assert(danbooru.hostname === "danbooru.donmai.us");
   });
 
-  it('should add date', () => {
+  it("should add date", () => {
     const opts = {
-      name: 'danbooru',
-      term: 'day',
+      name: "danbooru",
+      term: "day"
     };
     const danbooru = Mizu.createCrawler(opts);
-    assert(danbooru.next(1, 'day').date === moment().add(1, 'days').format('YYYY-MM-DD'));
-    assert(danbooru.next(1, 'week').date === moment().add(1, 'days').add(1, 'weeks').format('YYYY-MM-DD'));
-    assert(danbooru.next(1, 'month').date === moment().add(1, 'days').add(1, 'weeks').add(1, 'months').format('YYYY-MM-DD'));
+    assert(
+      danbooru.next(1, "day").date ===
+        moment().add(1, "days").format("YYYY-MM-DD")
+    );
+    assert(
+      danbooru.next(1, "week").date ===
+        moment().add(1, "days").add(1, "weeks").format("YYYY-MM-DD")
+    );
+    assert(
+      danbooru.next(1, "month").date ===
+        moment()
+          .add(1, "days")
+          .add(1, "weeks")
+          .add(1, "months")
+          .format("YYYY-MM-DD")
+    );
   });
 
-  it('should sbstract date', () => {
+  it("should sbstract date", () => {
     const opts = {
-      name: 'danbooru',
-      term: 'day',
+      name: "danbooru",
+      term: "day"
     };
     const danbooru = Mizu.createCrawler(opts);
-    assert(danbooru.prev(2, 'day').date === moment().subtract(2, 'days').format('YYYY-MM-DD'));
-    assert(danbooru.prev(2, 'week').date === moment().subtract(2, 'days').subtract(2, 'weeks').format('YYYY-MM-DD'));
-    assert(danbooru.prev(2, 'month').date === moment().subtract(2, 'days').subtract(2, 'weeks').subtract(2, 'months').format('YYYY-MM-DD'));
+    assert(
+      danbooru.prev(2, "day").date ===
+        moment().subtract(2, "days").format("YYYY-MM-DD")
+    );
+    assert(
+      danbooru.prev(2, "week").date ===
+        moment().subtract(2, "days").subtract(2, "weeks").format("YYYY-MM-DD")
+    );
+    assert(
+      danbooru.prev(2, "month").date ===
+        moment()
+          .subtract(2, "days")
+          .subtract(2, "weeks")
+          .subtract(2, "months")
+          .format("YYYY-MM-DD")
+    );
   });
 
-  it('should return daily illustList when pass {term: "day"}', (done) => {
+  it('should return daily illustList when pass {term: "day"}', done => {
     const opts = {
-      name: 'danbooru',
-      term: 'day',
+      name: "danbooru",
+      term: "day"
     };
     const danbooru = Mizu.createCrawler(opts);
-    danbooru.prev(1, 'day').crawl()
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].title));
-      done();
-    })
-    .catch( err => {
-      console.error(err);
-      done();
-    });
+    danbooru
+      .prev(1, "day")
+      .crawl()
+      .then(illustList => {
+        assert(_.isArray(illustList));
+        assert(_.isString(illustList[1].title));
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
 
-  it('should return weekly illustList when pass {term: "week"}', (done) => {
+  it('should return weekly illustList when pass {term: "week"}', done => {
     const opts = {
-      name: 'danbooru',
-      term: 'week',
-      directory: 'danbooru_test_images_week',
+      name: "danbooru",
+      term: "week",
+      directory: "danbooru_test_images_week"
     };
     const danbooru = Mizu.createCrawler(opts);
-    danbooru.prev(1, 'day').crawl()
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].title));
+    danbooru
+      .prev(1, "day")
+      .crawl()
+      .then(illustList => {
+        console.log(illustList);
+        assert(_.isArray(illustList));
+        assert(_.isString(illustList[1].title));
 
-      return danbooru.download();
-    })
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].filename) && !_.isEmpty(illustList[1].filename));
-      done();
-    })
-    .catch( err => {
-      console.error(err);
-      done();
-    });
+        return danbooru.download();
+      })
+      .then(illustList => {
+        assert(_.isArray(illustList));
+        assert(
+          _.isString(illustList[1].filename) &&
+            !_.isEmpty(illustList[1].filename)
+        );
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
 
-  it('should return monthly illustList when pass {term: "month"}', (done) => {
+  it('should return monthly illustList when pass {term: "month"}', done => {
     const opts = {
-      name: 'danbooru',
-      term: 'month',
+      name: "danbooru",
+      term: "month"
     };
     const danbooru = Mizu.createCrawler(opts);
-    danbooru.prev(1, 'month').crawl()
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].title));
-      done();
-    })
-    .catch( err => {
-      console.error(err);
-      done();
-    });
+    danbooru
+      .prev(1, "month")
+      .crawl()
+      .then(illustList => {
+        assert(_.isArray(illustList));
+        assert(_.isString(illustList[1].title));
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
 
-  it('should return 3 months ago illustList when pass {term: "month"} and call prev(3, "month")', (done) => {
+  it('should return 3 months ago illustList when pass {term: "month"} and call prev(3, "month")', done => {
     const opts = {
-      name: 'danbooru',
-      term: 'month',
+      name: "danbooru",
+      term: "month"
     };
     const danbooru = Mizu.createCrawler(opts);
-    danbooru.prev(3, 'month').crawl()
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].title));
-      return danbooru.download();
-    })
-    .then( illustList => {
-      assert(_.isArray(illustList));
-      assert(_.isString(illustList[1].filename) && !_.isEmpty(illustList[1].filename));
-      done();
-    })
-    .catch( err => {
-      console.error(err);
-      done();
-    });
+    danbooru
+      .prev(3, "month")
+      .crawl()
+      .then(illustList => {
+        assert(_.isArray(illustList));
+        assert(_.isString(illustList[1].title));
+        return danbooru.download();
+      })
+      .then(illustList => {
+        assert(_.isArray(illustList));
+        assert(
+          _.isString(illustList[1].filename) &&
+            !_.isEmpty(illustList[1].filename)
+        );
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
 });
