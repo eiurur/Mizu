84ad22b2b7cd19fd2757a727f299a961a07cb251
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-02-24 10:54:50 +0900

    [Fix] - fail to download images in yande.re

--- test/Yandere.spec.js
+++ test/Yandere.spec.js
@@ -6,20 +6,20 @@ const { testDownloading } = require('./helpers/testDownloading');
 test('should return daoly illustList when pass {term: "days"}', async (t) => {
   const options = { term: 'days' };
   const yandere = new Yandere(options);
-  const result = await yandere.download({ directory: './yandere_images_days', amount: 0 });
+  const result = await yandere.download({ directory: './yandere_images_days', amount: -1 });
   testDownloading(t, result);
 });
 
 test('should return weekly illustList when pass {term: "weeks"}', async (t) => {
   const options = { term: 'weeks' };
   const yandere = new Yandere(options);
-  const result = await yandere.download({ directory: './yandere_images_weeks', amount: 0 });
+  const result = await yandere.download({ directory: './yandere_images_weeks', amount: -1 });
   testDownloading(t, result);
 });
 
 test('should return monthly illustList when pass {term: "months"}', async (t) => {
   const options = { term: 'months' };
   const yandere = new Yandere(options);
-  const result = await yandere.download({ directory: './yandere_images_months', amount: 0 });
+  const result = await yandere.download({ directory: './yandere_images_months', amount: -1 });
   testDownloading(t, result);
 });
