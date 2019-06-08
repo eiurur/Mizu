d19c4f67a5ecfb9a7cada4cfe28d2ecae36a7366
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2019-06-09 01:51:04 +0900

    Reject test case of sankaku complex

--- test/SankakuComplex.spec.js
+++ test/SankakuComplex.spec.js
@@ -1,30 +1,30 @@
-const test = require('ava');
-const { SankakuComplex } = require('../');
-const { testDownloading } = require('./helpers/testDownloading');
+// const test = require('ava');
+// const { SankakuComplex } = require('../');
+// const { testDownloading } = require('./helpers/testDownloading');
 
-test('should return daoly illustList when pass {term: "days"}', async (t) => {
-  const initial = { term: 'days' };
-  const servive = new SankakuComplex(initial);
-  const result = await servive.download({ directory: './sankaku_complex_images_days', amount: -1 });
-  testDownloading(t, result);
-});
+// test('should return daoly illustList when pass {term: "days"}', async (t) => {
+//   const initial = { term: 'days' };
+//   const servive = new SankakuComplex(initial);
+//   const result = await servive.download({ directory: './sankaku_complex_images_days', amount: -1 });
+//   testDownloading(t, result);
+// });
 
-test('should return weekly illustList when pass {term: "weeks"}', async (t) => {
-  const initial = { term: 'weeks' };
-  const servive = new SankakuComplex(initial);
-  const result = await servive.download({
-    directory: './sankaku_complex_images_weeks',
-    amount: -1,
-  });
-  testDownloading(t, result);
-});
+// test('should return weekly illustList when pass {term: "weeks"}', async (t) => {
+//   const initial = { term: 'weeks' };
+//   const servive = new SankakuComplex(initial);
+//   const result = await servive.download({
+//     directory: './sankaku_complex_images_weeks',
+//     amount: -1,
+//   });
+//   testDownloading(t, result);
+// });
 
-test('should return monthly illustList when pass {term: "months"}', async (t) => {
-  const initial = { term: 'months' };
-  const servive = new SankakuComplex(initial);
-  const result = await servive.download({
-    directory: './sankaku_complex_images_months',
-    amount: -1,
-  });
-  testDownloading(t, result);
-});
+// test('should return monthly illustList when pass {term: "months"}', async (t) => {
+//   const initial = { term: 'months' };
+//   const servive = new SankakuComplex(initial);
+//   const result = await servive.download({
+//     directory: './sankaku_complex_images_months',
+//     amount: -1,
+//   });
+//   testDownloading(t, result);
+// });
