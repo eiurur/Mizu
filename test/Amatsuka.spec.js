8e313ba2918bb1622944aadf9b72a31354d69854
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-07-13 23:52:43 +0900

    enable amatsuka

--- test/Amatsuka.spec.js
+++ test/Amatsuka.spec.js
@@ -2,52 +2,70 @@ const test = require('ava');
 const { Amatsuka } = require('../');
 const { testDownloading } = require('./helpers/_checker');
 
-test('WIP', t => {
+test('WIP', (t) => {
   t.pass();
 });
 
-// TODO
+test('should return daoly illustList when pass {term: "days",  sort: "like"}', async (t) => {
+  const options = { term: 'days' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_days',
+    amount: -1,
+    options: { sort: 'like' },
+  });
+  testDownloading(t, result);
+});
+test('should return weekly illustList when pass {term: "weeks", sort: "like"}', async (t) => {
+  const options = { term: 'weeks' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_weeks_like',
+    amount: -1,
+    options: { sort: 'like' },
+  });
+  testDownloading(t, result);
+});
+test('should return weekly illustList when pass {term: "weeks"}', async (t) => {
+  const options = { term: 'weeks' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_weeks_retweer',
+    amount: -1,
+    options: { sort: 'retweet' },
+  });
+  testDownloading(t, result);
+});
 
-// test('should return daoly illustList when pass {term: "days"}', async (t) => {
-//   const options = { term: 'days' };
-//   const servive = new Amatsuka(options);
-//   const result = await servive.download({
-//     directory: './amatsuka_images_days',
-//     amount: -1,
-//     options: { sort: 'like' },
-//   });
-//   testDownloading(t, result);
-// });
+test('should return monthly illustList when pass {term: "months"}', async (t) => {
+  const options = { term: 'months' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_months_lustful',
+    amount: -1,
+    options: { sort: 'lustful' },
+  });
+  testDownloading(t, result);
+});
 
-// test('should return weekly illustList when pass {term: "weeks"}', async (t) => {
-//   const options = { term: 'weeks' };
-//   const servive = new Amatsuka(options);
-//   const result = await servive.download({
-//     directory: './amatsuka_images_weeks',
-//     amount: -1,
-//     options: { sort: 'retweet' },
-//   });
-//   testDownloading(t, result);
-// });
+test('should return monthly illustList when pass {term: "months"} and {options: {sort: "total"} }', async (t) => {
+  const options = { term: 'months' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_months_total',
+    amount: -1,
+    options: { sort: 'total' },
+  });
+  testDownloading(t, result);
+});
 
-// test('should return monthly illustList when pass {term: "months"}', async (t) => {
-//   const options = { term: 'months' };
-//   const servive = new Amatsuka(options);
-//   const result = await servive.download({
-//     directory: './amatsuka_images_months',
-//     amount: -1,
-//     options: { sort: 'lustful' },
-//   });
-//   testDownloading(t, result);
-// });
-
-// test('should return monthly illustList when pass {term: "months"} and {options: {sort: "total"} }', async (t) => {
-//   const options = { term: 'months' };
-//   const servive = new Amatsuka(options);
-//   const result = await servive.download({
-//     directory: './amatsuka_images_months',
-//     amount: -1,
-//     options: { sort: 'total' },
-//   });
-//   testDownloading(t, result);
-// });
+test('should return monthly illustList when pass {term: "months"} and {options: {sort: "like"} }', async (t) => {
+  const options = { term: 'months' };
+  const servive = new Amatsuka(options);
+  const result = await servive.download({
+    directory: './amatsuka_images_months_like',
+    amount: -1,
+    options: { sort: 'like' },
+  });
+  testDownloading(t, result);
+});
