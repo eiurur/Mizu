42374e9d5b75ab8336d84a6015d51a6364d1d012
eiurur <eiurur789@gmail.com>
2021-12-19 12:36:19 +0900

    omit amatsuka crawler

--- test/Amatsuka.spec.js
+++ test/Amatsuka.spec.js
@@ -6,66 +6,66 @@ test('WIP', (t) => {
   t.pass();
 });
 
-test('should return daoly illustList when pass {term: "days",  sort: "like"}', async (t) => {
-  const options = { term: 'days', date: '2020-06-21' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_days',
-    amount: -1,
-    options: { sort: 'like', limit: 10 },
-  });
-  testDownloading(t, result);
-});
-test('should return weekly illustList when pass {term: "weeks", sort: "like"}', async (t) => {
-  const options = { term: 'weeks' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_weeks_like',
-    amount: -1,
-    options: { sort: 'like', limit: 10 },
-  });
-  testDownloading(t, result);
-});
-test('should return weekly illustList when pass {term: "weeks", sort: "retweet"}', async (t) => {
-  const options = { term: 'weeks' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_weeks_retweet',
-    amount: -1,
-    options: { sort: 'retweet', limit: 10 },
-  });
-  testDownloading(t, result);
-});
+// test('should return daoly illustList when pass {term: "days",  sort: "like"}', async (t) => {
+//   const options = { term: 'days', date: '2020-06-21' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_days',
+//     amount: -1,
+//     options: { sort: 'like', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
+// test('should return weekly illustList when pass {term: "weeks", sort: "like"}', async (t) => {
+//   const options = { term: 'weeks' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_weeks_like',
+//     amount: -1,
+//     options: { sort: 'like', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
+// test('should return weekly illustList when pass {term: "weeks", sort: "retweet"}', async (t) => {
+//   const options = { term: 'weeks' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_weeks_retweet',
+//     amount: -1,
+//     options: { sort: 'retweet', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
 
-test('should return monthly illustList when pass {term: "months"}', async (t) => {
-  const options = { term: 'months' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_months_lustful',
-    amount: -1,
-    options: { sort: 'lustful', limit: 10 },
-  });
-  testDownloading(t, result);
-});
+// test('should return monthly illustList when pass {term: "months"}', async (t) => {
+//   const options = { term: 'months' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_months_lustful',
+//     amount: -1,
+//     options: { sort: 'lustful', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
 
-test('should return monthly illustList when pass {term: "months"} and {options: {sort: "total"} }', async (t) => {
-  const options = { term: 'months' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_months_total',
-    amount: -1,
-    options: { sort: 'total', limit: 10 },
-  });
-  testDownloading(t, result);
-});
+// test('should return monthly illustList when pass {term: "months"} and {options: {sort: "total"} }', async (t) => {
+//   const options = { term: 'months' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_months_total',
+//     amount: -1,
+//     options: { sort: 'total', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
 
-test('should return monthly illustList when pass {term: "months"} and {options: {sort: "like"} }', async (t) => {
-  const options = { term: 'months' };
-  const servive = new Amatsuka(options);
-  const result = await servive.download({
-    directory: './amatsuka_images_months_like',
-    amount: -1,
-    options: { sort: 'like', limit: 10 },
-  });
-  testDownloading(t, result);
-});
+// test('should return monthly illustList when pass {term: "months"} and {options: {sort: "like"} }', async (t) => {
+//   const options = { term: 'months' };
+//   const servive = new Amatsuka(options);
+//   const result = await servive.download({
+//     directory: './amatsuka_images_months_like',
+//     amount: -1,
+//     options: { sort: 'like', limit: 10 },
+//   });
+//   testDownloading(t, result);
+// });
